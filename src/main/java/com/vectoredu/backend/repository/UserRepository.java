3e3a1340a8a8ec13655d68ebffb00710bc8a18d6
aorandomcrewSSS <danbogomolovhr@gmai.com>
2025-03-17 01:47:34 +0300

    some-fixes

--- src/main/java/com/vectoredu/backend/repository/UserRepository.java
+++ src/main/java/com/vectoredu/backend/repository/UserRepository.java
@@ -1,7 +1,9 @@
 package com.vectoredu.backend.repository;
 
 import com.vectoredu.backend.model.User;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -15,6 +17,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByVerificationCode(String verificationCode);
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("SELECT u FROM User u LEFT JOIN FETCH u.courses WHERE u.email = :email")
     Optional<User> findByEmailWithCourses(@Param("email") String email);
 }
